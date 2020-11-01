020f1387469faa3f063672a89cfe78a237947d66
bomotayo <omotayobabatundee@gmail.com>
2020-11-01 11:32:27 -0500

    moved routes from app to its own file in components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Container } from "semantic-ui-react";
 
-import CreateEvent from "./pages/CreateEvent";
-import CreateGroup from "./pages/CreateGroup";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-
-import Navigation from "./components/Navigation";
+import Routes from "./components/Routes";
 
 const App = () => {
-  return (
-    //Routes + Navigation
-    <Router>
-      <Container>
-        <Navigation />
-        <Route exact path="/" component={Home} />
-        <Route path="/create-group" component={CreateGroup} />
-        <Route path="/create-event" component={CreateEvent} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-      </Container>
-    </Router>
-  );
+  return <Routes />;
 };
 
 export default App;
---
+++ src/components/Routes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import CreateEvent from "../pages/CreateEvent";
+import CreateGroup from "../pages/CreateGroup";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+
+import Navigation from "./Navigation";
+
+const Routes = () => {
+  return (
+    //Routes + Navigation
+    <Router>
+      <Container>
+        <Navigation />
+        <Route exact path="/" component={Home} />
+        <Route path="/create-group" component={CreateGroup} />
+        <Route path="/create-event" component={CreateEvent} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+      </Container>
+    </Router>
+  );
+};
+
+export default Routes;
